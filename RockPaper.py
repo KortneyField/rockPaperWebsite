e006c7afd51e15200e07bf53ce671d55896ca216
Kortney <kfiel48@wgu.edu>
2020-03-31 16:59:51 -0500

    added images and img vs img

--- RockPaper.py
+++ RockPaper.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+
+import random
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,18 +11,42 @@ def home_page():
 def new_function():
     return "hello hello"
 
-@app.route('/pickRock')
-def buildMyPage():
-    output = 0
-    number_list = [1,2,3,4,5]
-    for i in number_list:
-        output += i
-    return render_template("rock.html", numberSum=output)
+SELECTIONS_LIST_RPS = ['ROCK', 'PAPER', 'SCISSORS']
+selections_list = SELECTIONS_LIST_RPS
+WIN_LIST_RPS = [('ROCK', 'SCISSORS'),
+                ('SCISSORS', 'PAPER'),
+                ('PAPER', 'ROCK')]
 
-@app.route('/pickPaper')
-def buildMyPaperPage():
-    return render_template("paper.html")
 
-@app.route('/pickScissors')
-def buildMyScissorPage():
-    return render_template("scissors.html")
+def computer_selection():
+    computer_selection = random.choice(selections_list)
+    return computer_selection
+
+@app.route('/playGame/<user_input>')
+def buildMyPage(user_input):
+    computer = computer_selection()
+    picked=user_input.upper()
+    result = results(picked, computer)
+    
+    return render_template("playGame.html", pick=picked, comp=computer, result=result)
+
+def results(user, computer):
+    if user==computer:
+        return "TIE!! Play again!"
+    if user== "ROCK": 
+        if computer == "PAPER":
+            return "Computer Won"
+        else: 
+            return "You Won! Congrats!"
+    if user == "PAPER":
+        if computer == "SCISSORS":
+            return "Computer Won"
+        else: 
+            return "You Won! Congrats!"
+    if user == "SCISSORS":
+        if computer == "ROCK":
+            return "Computer Won"
+        else: 
+            return "You Won! Congrats!"
+        return "Testing"
+
